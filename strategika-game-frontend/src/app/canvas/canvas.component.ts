42cfad368bbb91025c6a8577170d5b3819f3beb5
Krzysztof Pająk <chris.araneo@gmail.com>
2024-01-13 19:47:03 +0100

    Implement basic canvas init

--- strategika-game-frontend/src/app/canvas/canvas.component.ts
+++ strategika-game-frontend/src/app/canvas/canvas.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
-  selector: 'app-canvas',
+  selector: 'stg-canvas',
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.scss'],
 })
-export class CanvasComponent {}
+export class CanvasComponent {
+  @ViewChild('canvas')
+  canvas!: ElementRef<HTMLCanvasElement>;
+
+  context: CanvasRenderingContext2D | null = null;
+
+  constructor(private readonly viewContainerRef: ViewContainerRef) {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    this.resizeCanvas();
+    this.render();
+  }
+
+  ngAfterViewInit(): void {
+    this.initializeContext();
+    this.resizeCanvas();
+    this.render();
+  }
+
+  private initializeContext(): void {
+    this.context = this.canvas.nativeElement.getContext('2d');
+  }
+
+  private resizeCanvas(): void {
+    const { clientWidth, clientHeight } = this.viewContainerRef.element.nativeElement;
+
+    this.canvas.nativeElement.width = clientWidth;
+    this.canvas.nativeElement.height = clientHeight;
+  }
+
+  // TODO Example rendering
+  private render(): void {
+    if (this.context !== null) {
+      this.context.rect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+      this.context.fillStyle = 'lightgreen';
+      this.context.fill();
+    } else {
+      throw Error('Context is empty');
+    }
+  }
+}
